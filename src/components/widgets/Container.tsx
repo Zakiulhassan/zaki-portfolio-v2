5a8f6d6d021e7a4a33ee21cc26f4756f321b4b55
= <zakihassan555@gmail.com>
2024-09-27 12:18:03 +0500

    fonts and color variables added

---
+++ src/components/widgets/Container.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface ContainerProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Container: React.FC<ContainerProps> = ({ children, className = '' }) => {
+  return (
+    <div className={`w-full px-container md:px-container-md lg:px-container-lg xl:px-container-xl ${className}`}>
+      {children}
+    </div>
+  );
+};
+
+export default Container;
