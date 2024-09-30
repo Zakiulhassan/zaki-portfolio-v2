d09086a5c1112073d52b63410b499aaa7f43ed34
= <zakihassan555@gmail.com>
2024-09-30 18:18:38 +0500

    Project Plans Section, Case Study Section, Testimonials Section, Footer

--- src/components/home/TestimonialsSection.tsx
+++ src/components/home/TestimonialsSection.tsx
@@ -10,8 +10,8 @@ const TestimonialsSection = () => {
                 <h1 className='text-3xl text-center font-bricolage font-bold leading-tight'>Testimonial from peers & coworkers</h1>
                 <p className='text-base text-center font-jakarta leading-tight text-secondary'>A few kind words people have to say about collaborating and solving problems with me.</p>
             </div>
-        </Container>
         <Testimonials/>
+        </Container>
     </section>
   )
 }
